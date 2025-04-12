686ccb6cd910ee6bcf5496d1a150889b3a63705c
toshan292003 <toshanyt@gmail.com>
2025-04-12 15:16:23 +0530

    cleanup

--- frontend/src/global_components/TestCard.jsx
+++ frontend/src/global_components/TestCard.jsx
@@ -1,25 +1,3 @@
-// import React from "react";
-// import styles from "./testcard.module.css"
-// import { useNavigate } from "react-router-dom"
-
-// export default function TestCard({ test, classname }) {
-//     console.log(test)
-//     const navigate = useNavigate()
-//     const hoverEvent = (e) => {
-//         e.preventDefault();
-//     }
-//     const openTest = () => {
-//         const url = `/asessment?id=${test._id}`
-//         navigate(url)
-//     }
-//     return (
-//         <>
-//             <div className={`${styles.card} ${classname}`} onClick={openTest} onMouseEnter={hoverEvent} >
-//                 <span>{test.testName}</span>
-//             </div>
-//         </>
-//     )
-// }
 import React, { useState } from "react";
 import styles from "./testcard.module.css";
 import { useNavigate } from "react-router-dom";
